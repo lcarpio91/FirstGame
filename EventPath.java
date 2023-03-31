a21e20da2a6e2255b8605be358d57bf24d85ceeb
LeeRoy <80301370+lcarpio91@users.noreply.github.com>
2023-03-31 08:26:18 -0500

    Event path
    File was not save. Saved file and upload

--- EventPath.java
+++ EventPath.java
@@ -17,10 +17,7 @@ public class EventPath {
 			   + "\nThe Minotaur snorts, tosses the body aside, turns in your direction stompign it's hooves and letting our a roar"
 			   + "\nYou step out into the sunlight from and you...";
 		}
-//		public void minotaurFight() {
-//			if ()
-//			
-//		}
+
 		
 		public String bridgeTroll() {
 			return "The Sun is setting."
@@ -34,19 +31,36 @@ public class EventPath {
 				+ "\nYou draw your weapon while complementing to run and you...";
 		}
 		public String forestElves() {
-			return "forestElves";
+			return "You finally find an area to rest at while travling through WhiteWood forest."
+					+ "\nAs you set up camp, an arrow hits your tent. You turn around quickly and see a slender figure "
+					+ "\nwith a beautiful bow, a Forest Elf. You soon realize it is not one elf. But several!"
+					+ "\nYou collect your things and begin to leave when another arrow is shot. You are ables to roll out of the way."
+					+ "\nYou drop your belongs and you... ";
 		}
 		
 		public String caveGoblins() {
-			return "caveGoblins";
+			return "You find abondoned home and search through it looking for food and any essential items. The floor creaks"
+					+ "\nunder your weight and make your way to the center of the living room. The floor breaks and fall into a tunnel."
+					+ "\nYou are too far down to go back up and you make your way through the tunnel. You reach the end and walk into a"
+					+ "\nCave dweling. You can hear in the distance - screams, clanking of metal, Cave Goblins. You see a way to leave "
+					+ "\but as you are running for the exit. You hear the clanking get louder! You get to exit but it to high to reach."
+					+ "\nYou turn around and you... ";
 		}
 		
 		public String castleDragon() {
-			return "castleDragon";
+			return "You see in the distance what appears to be a Castle with the land scourched around it. You continue to walk "
+					+ "\nbut the land is in terrible condition. Homes, farm animals, crops all burnt down. You look at the castle and "
+					+ "\na giant, red scaly head appears. It climbs to perch it self on the castle wall, spreads its wings and roars a thunderous "
+					+ "\nRoar! You feel your heart skip a few beats. You gather yourself and you ...";
 		}
 		
 		public String mountainGolem() {
-			return "mountainGolem";
+			return "You are walking through a mountain range named Stonefall. There is so much peach here and it feels good."
+					+ "\nYou come to the valley and there are no animals or birds in the area. Just a wide open field."
+					+ "\nYou are looking around, enjoying the peace when you come upon a grass covered boulder. You climb it,"
+					+ "\nsit on it to enjoy the sun. The feel the boulder shake and are then thrown several feet away. "
+					+ "\nYou quickly stand up and still trying to process what happened. You see the boulder begin to stand up,"
+					+ "a Golem. You draw your weapon and you ...";
 		}
 		
 		// Death path
